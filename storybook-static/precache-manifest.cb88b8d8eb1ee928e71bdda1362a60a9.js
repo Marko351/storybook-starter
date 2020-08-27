e7c21453f0dd8178f21a3bdad45532247c9c37c1
Marko351 <markodjuric351@gmail.com>
2020-08-27 17:30:40 +0200

    taskbox UI

---
+++ storybook-static/precache-manifest.cb88b8d8eb1ee928e71bdda1362a60a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f27128da296cfe893983ddc61bce4c",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.01e1dcfadd976a32df72.bundle.js"
+  },
+  {
+    "url": "runtime~main.01e1dcfadd976a32df72.bundle.js"
+  },
+  {
+    "revision": "a1d0c8f507642c325902",
+    "url": "static/css/main.2c553ac5.chunk.css"
+  },
+  {
+    "url": "vendors~main.01e1dcfadd976a32df72.bundle.js"
+  },
+  {
+    "url": "vendors~main.01e1dcfadd976a32df72.bundle.js.LICENSE.txt"
+  }
+]);
